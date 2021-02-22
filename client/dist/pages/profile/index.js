bdee402aa2328d04cb078b5a074cf9ac0fe230e9
Kamil Khismatullin <kami16ru@gmail.com>
2021-02-22 18:12:34 +0300

    YC-29 change avatar dialog

--- client/dist/pages/profile/index.js
+++ client/dist/pages/profile/index.js
@@ -112,3 +112,30 @@ function submitProfileEditPasswordForm() {
         }, 2000);
     }
 }
+
+
+// Dialog module
+
+// Get the modal
+const dialog = document.getElementById("dialog");
+
+// Get the button that opens the modal
+const dialogOpen = document.getElementById("avatar-change");
+const dialogClose = document.getElementsByClassName("dialog-close")[0];
+
+// When the user clicks the button, open the modal
+dialogOpen.onclick = function() {
+    dialog.style.display = "flex";
+}
+
+// When the user clicks on <span> (x), close the modal
+dialogClose.onclick = function() {
+    dialog.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target === dialog) {
+        dialog.style.display = "none";
+    }
+}
