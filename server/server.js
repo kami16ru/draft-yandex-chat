c70558d74cf935c2a36d0239e968b82d8df33c76
Kamil Khismatullin <kami16ru@gmail.com>
2021-05-31 22:27:52 +0300

    port changed

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 4000;
+const PORT = 3000;
 const clientPath = path.join(__dirname, '../client/dist');
 
 app.use(express.static(clientPath));
