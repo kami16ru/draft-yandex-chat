d295e8b53d2522160049d7ee1c8c7010637e95ce
Kamil Khismatullin <kami16ru@gmail.com>
2021-06-08 09:55:43 +0300

    eslint added

--- dist2/pages/profile/profile.js
+++ dist2/pages/profile/profile.js
@@ -1,13 +1,13 @@
 function logout() {
-    window.location.href = '/';
+  window.location.href = '/';
 }
 
 function showEditImg() {
-    document.getElementById('avatar').setAttribute('hidden', 'true');
-    document.getElementById('avatar-change').removeAttribute('hidden');
+  document.getElementById('avatar').setAttribute('hidden', 'true');
+  document.getElementById('avatar-change').removeAttribute('hidden');
 }
 
 function hideEditImg() {
-    document.getElementById('avatar-change').setAttribute('hidden', 'true');
-    document.getElementById('avatar').removeAttribute('hidden');
+  document.getElementById('avatar-change').setAttribute('hidden', 'true');
+  document.getElementById('avatar').removeAttribute('hidden');
 }
